132b2365ebcda7b4ebde8c976a1b0d88e7378ee4
Juan <juan.alesandri.acc@gmail.com>
2022-08-25 10:14:28 -0300

    Creacion de directiva personalizada usando renderizador para acceder a elementos del DOM

--- directives-start/src/app/app.module.ts
+++ directives-start/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { BasicHighLight } from './basic-highlight/basic-highlight.directive';
+import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighLight,
+    BetterHighlightDirective,
   ],
   imports: [
     BrowserModule,
