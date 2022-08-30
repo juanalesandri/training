6fb64b039381ec75d116f4ce9d8f122baceb678d
Juan <juan.alesandri.acc@gmail.com>
2022-08-30 17:04:46 -0300

    Seccion 13 finalizada

--- obs-01-start/src/app/app.component.ts
+++ obs-01-start/src/app/app.component.ts
@@ -1,12 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from './user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+  userActivated: boolean = false;
+  private activatedSub: Subscription;
 
-  ngOnInit() {}
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    this.activatedSub = this.userService.activatedEmitter.subscribe(isActivate => {
+      this.userActivated = isActivate;
+    })
+  }
+
+  ngOnDestroy(): void {
+    this.activatedSub.unsubscribe();
+  }
 }
