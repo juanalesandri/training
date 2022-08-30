4deba351578492bf8ac6cf604978b88ff0ed2062
Juan <juan.alesandri.acc@gmail.com>
2022-08-30 10:11:25 -0300

    Se agrega enrutamiento

---
+++ recipe-book-shopping-list-app/src/app/app-routing.modules.ts
@@ -0,0 +1,23 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
+import { RecipesComponent } from "./recipes/recipes.component";
+import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
+
+const appRoutes: Routes = [
+    { path: '', redirectTo: '/recipes', pathMatch: 'full' },
+    //{ path: 'home', component: AppComponent },
+    { path: 'shopping-list', component: ShoppingListComponent },
+    {
+        path: 'recipes', component: RecipesComponent
+    },
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(appRoutes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
