f6861b692335a15a29e9788492fb070d614b2949
Juan <juan.alesandri.acc@gmail.com>
2022-08-31 10:25:14 -0300

    Se agrega observable Subject en lugar de EventEmitter

--- recipe-book-shopping-list-app/src/app/recipes/recipes.service.ts
+++ recipe-book-shopping-list-app/src/app/recipes/recipes.service.ts
@@ -1,11 +1,10 @@
-import { EventEmitter, Injectable } from "@angular/core";
+import { Injectable } from "@angular/core";
 import { Ingredient } from "../shared/ingredient.model";
 import { ShoppingListService } from "../shopping-list/shopping-list.service";
 import { Recipe } from "./recipe.model";
 
 @Injectable()
 export class RecipeService {
-    recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://thebrilliantkitchen.com/wp-content/uploads/2022/08/Lamb-Medium-Rare%E2%80%8B%E2%80%8B.jpg', [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]),
