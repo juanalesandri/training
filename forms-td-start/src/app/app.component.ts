9b4b84617ef419d571decded86710b5cc88f6aa3
Juan <juan.alesandri.acc@gmail.com>
2022-08-31 16:38:27 -0300

    Fin formularios basados en plantilla

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
 @Component({
@@ -7,11 +7,53 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  @ViewChild('form') signupForm: NgForm;
+  selectQuestion: string = 'pet';
+  answer: string = '';
+  genders: string[] = ['male', 'female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  }
+  submitted: boolean = false;
+
   suggestUserName() {
     const suggestedName = 'Superuser';
+
+    //PARA SOBRESCRIBIR TODO EL FORMULARIO
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secret: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // })
+
+    //PARA SOBREESCRIBIR SOLO LO QUE ESTABLESCO
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
-  submitForm(form: NgForm) {
-    console.log(form);
+  // submitForm(form: NgForm) {
+  //   console.log(form);
+  // }
+  submitForm() {
+    console.log(this.signupForm);
+    this.submitted = !this.submitted;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this.signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
