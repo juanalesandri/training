8c75cd566632ffc708d4040fa11a0750cad6911d
Juan <juan.alesandri.acc@gmail.com>
2022-09-01 13:12:36 -0300

    Creacion de nuevo proyecto que contiene formularios reactivos y template driven

---
+++ forms-start/src/app/reactive-form/reactive-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-form',
+  templateUrl: './reactive-form.component.html',
+  styleUrls: ['./reactive-form.component.css']
+})
+export class ReactiveFormComponent implements OnInit {
+  genders = ['male', 'female'];
+  signupForm: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.signupForm = new FormGroup({
+      'username': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'gender': new FormControl('male')
+    })
+  }
+
+  submitForm() {
+    console.log(this.signupForm)
+  }
+
+}
