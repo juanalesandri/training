f9a7d763cb37710ccb6d439673cd4cee2d827ea5
Juan <juan.alesandri.acc@gmail.com>
2022-09-06 12:40:22 -0300

    Se agrega async pipe

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -6,6 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  filteredStatus: string = '';
+
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 5000)
+  })
+
   servers = [
     {
       instanceType: 'medium',
@@ -32,11 +40,22 @@ export class AppComponent {
       started: new Date(15, 1, 2017)
     }
   ];
+
   getStatusClasses(server: { instanceType: string, name: string, status: string, started: Date }) {
     return {
       'list-group-item-success': server.status === 'stable',
       'list-group-item-warning': server.status === 'offline',
       'list-group-item-danger': server.status === 'critical'
     };
+  }
+
+  addServer() {
+    this.servers
+      .push({
+        instanceType: 'small',
+        name: 'New Server',
+        status: 'stable',
+        started: new Date(15, 1, 2017)
+      })
   }
 }
