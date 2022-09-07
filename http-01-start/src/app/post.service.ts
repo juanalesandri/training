6375c3fc486e033f0ad7c07769e28d92ea573981
Juan <juan.alesandri.acc@gmail.com>
2022-09-07 10:02:52 -0300

    Se agrega manejo de errores. Iniciacion a los headers

--- http-01-start/src/app/post.service.ts
+++ http-01-start/src/app/post.service.ts
@@ -1,26 +1,46 @@
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { map } from 'rxjs/operators';
+import { Subject, throwError } from "rxjs";
+import { map, catchError } from 'rxjs/operators';
 import { Post } from "./post.model";
 
 @Injectable({ providedIn: 'root' })
 export class PostService {
     private urlBase = 'https://curso-angular-36237-default-rtdb.firebaseio.com/';
+    error = new Subject<string>();
+    headers = {
+        headers: new HttpHeaders({
+            'Custom-Header': 'hola'
+        })
+    };
+
     constructor(private http: HttpClient) { }
 
+    // creteNewPost(data: Post) {
+    //     return this.http.post<{ name: string }>(`${this.urlBase}posts.json`, data);
+    // }
+
     creteNewPost(data: Post) {
-        return this.http.post<{ name: string }>(`${this.urlBase}posts.json`, data);
+        this.http.post<{ name: string }>(`${this.urlBase}posts.json`, data).subscribe(resp => {
+            return resp;
+        }, error => this.error.next(error.error.error));
     }
 
     getPosts() {
-        return this.http.get<{ [key: string]: Post }>(`${this.urlBase}posts.json`).pipe(map(respData => {
-            const postsArray: Post[] = [];
-            for (const key in respData) {
-                if (respData.hasOwnProperty(key))
-                    postsArray.push({ ...respData[key], id: key });
-            }
-            return postsArray;
-        }))
+        return this.http.get<{ [key: string]: Post }>(`${this.urlBase}posts.json`, this.headers)
+            .pipe(
+                map(respData => {
+                    const postsArray: Post[] = [];
+                    for (const key in respData) {
+                        if (respData.hasOwnProperty(key))
+                            postsArray.push({ ...respData[key], id: key });
+                    }
+                    return postsArray;
+                }),
+                catchError(errorResp => {
+                    return throwError(errorResp);
+                })
+            );
     }
 
     deletePosts() {
