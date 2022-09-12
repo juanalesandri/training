f56b9fb20e16532872f6232eda09d4d93e8d5578
Juan <juan.alesandri.acc@gmail.com>
2022-09-12 11:59:30 -0300

    Creacion de modulo con componentes compartidos

--- recipe-book-shopping-list-app/src/app/recipes/recipes.module.ts
+++ recipe-book-shopping-list-app/src/app/recipes/recipes.module.ts
@@ -1,7 +1,7 @@
-import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeHomeComponent } from "./recipe-home/recipe-home.component";
@@ -21,9 +21,9 @@ import { RecipesComponent } from "./recipes.component";
     ],
     imports: [
         RouterModule,
-        CommonModule,
         ReactiveFormsModule,
         RecipesRoutingModule,
+        SharedModule
     ],
     // exports: [
     //     RecipesComponent,
