6319790d4576982ad5a36254156958928781c86f
Juan <juan.alesandri.acc@gmail.com>
2022-09-15 09:15:07 -0300

    Inicio seccion angular universal

--- recipe-book-shopping-list-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ recipe-book-shopping-list-app/src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Supermarket } from 'src/app/supermarkets/supermarket.model';
+import { SupermarketService } from 'src/app/supermarkets/supermarket.service';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipes.service';
 
@@ -11,11 +14,15 @@ import { RecipeService } from '../recipes.service';
 export class RecipeDetailComponent implements OnInit {
   recipe: Recipe;
   id: number;
+  showSupermarkets: boolean;
+  supermarkets: Supermarket[] = [];
+  ingredientToSearch: string = '';
 
-  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router) { }
+  constructor(private recipeService: RecipeService, private route: ActivatedRoute, private router: Router, private supermarketService: SupermarketService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
+      this.showSupermarkets = false;
       this.id = parseInt(params.id);
       this.recipe = this.recipeService.getRecipe(this.id)
     });
@@ -33,6 +40,21 @@ export class RecipeDetailComponent implements OnInit {
   deleteRecipe() {
     this.recipeService.deleteRecipe(this.id);
     this.router.navigate(['/recipes']);
+  }
+
+  searchIngredient(ingredient: Ingredient) {
+    this.ingredientToSearch = ingredient.name;
+    // this.supermarketService.getSupermarketByProducts(ingredient.name).subscribe(supermarkets => {
+
+    // })
+    this.supermarketService.getAllSupermarkets().subscribe(supermarkets => {
+      this.showSupermarkets = true;
+      this.supermarkets = supermarkets.filter(supermarket => {
+        return supermarket.products.some(product => product.name.toLowerCase() === this.ingredientToSearch.toLowerCase());
+      });
+      console.log('ingrediente', this.ingredientToSearch)
+      console.log('ssssss', this.supermarkets)
+    })
   }
 
 }
